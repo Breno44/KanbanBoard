55236bd25f421ddd2ae698ccbac3539215ad4a04
Breno Henrique <bhfs.2908@gmail.com>
2021-09-16 16:00:00 -0300

    feat:add logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { FaMagento } from "react-icons/fa";
 
 const App = () => {
   return (
     <div className="h-screen flex">
-      <div className="w-64 bg-gray-100 border-r"></div>
+      <div className="w-64 px-8 py-4 bg-gray-100 border-r">
+        <FaMagento className="h-9 w-9" />
+        <nav>
+          <h2>Issues</h2>
+        </nav>
+      </div>
       <div className="bg-white">
         <div className="border-b-2 border-gray-200"></div>
         <div></div>
