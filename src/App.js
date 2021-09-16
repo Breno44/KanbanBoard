6c2a2b8d5cfdfe647cd253ea07988e788c7ad492
Breno Henrique <bhfs.2908@gmail.com>
2021-09-16 13:11:06 -0300

    feat:add dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-screen flex">
+      <div className="w-64 bg-gray-100 border-r"></div>
+      <div className="bg-white">
+        <div className="border-b-2 border-gray-200"></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
